87e5e95ac2dd707e8f29b8e706be0fd008742d37
Anh Nguyen <anhhao1102@gmail.com>
2016-09-02 16:32:40 -0600

    Uri.Builder

--- notes.java
+++ notes.java
@@ -2,6 +2,8 @@
 //Inside onCreate of this fragment: 
 setHasOptionsMenu(true) 
 
+---------------
+
 //Build URL with parameter (not concatenate strings)
 //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&APPID=85cf992512a687d70fae2e87edef2a4e&mode=json&cnt=7&units=metric");
 Uri.Builder builder = new Uri.Builder();
@@ -14,3 +16,5 @@ builder.scheme("http")
 	.appendQueryParameter("cnt", "7")
 	.appendQueryParameter("units", "metric");
 URL url = new URL(builder.build().toString());
+
+----------------
